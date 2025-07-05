aa692975583b5a37084508f79664f8451f4aaea3
Devill0104 <adityamaurya533@gmail.com>
2025-07-05 12:01:19 +0530

    same site

--- BACKEND/src/config/config.js
+++ BACKEND/src/config/config.js
@@ -4,7 +4,8 @@ export const cookieOptions = {
     maxAge: 1000 * 60 * 60, 
     httpOnly: true,
     secure:false, 
-    sameSite: 'lax', 
+    // sameSite: 'lax', 
+    sameSite: 'none', 
 }
 export const verifyToken = (token) => {
     return jsonwebtoken.verify(token, process.env.JWT_SECRET)
