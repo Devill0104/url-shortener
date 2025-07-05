e21b124d622a4019d0231217e891b21abb5a81f7
Devill0104 <adityamaurya533@gmail.com>
2025-07-05 15:56:35 +0530

    adddin only shorturl additiion

--- FRONTEND/src/api/shortUrl.api.js
+++ FRONTEND/src/api/shortUrl.api.js
@@ -4,6 +4,7 @@ import axiosInstance from "../utils/axiosInstance.js";
 const createShortUrl = async (url, slug) => {
     
         const {data} = await axiosInstance.post('/api/create', { url , slug}, { withCredentials: true });
+        console.log("data ;" ,data)
         return data.shortUrl;
   
 };
