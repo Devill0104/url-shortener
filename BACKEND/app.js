88f72b158a6457ff7db42b87fff5b653ae3757b4
Devill0104 <adityamaurya533@gmail.com>
2025-07-05 14:52:45 +0530

    adddin only shorturl additiion

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 import dotenv from 'dotenv'
 import connectDb from './src/config/mongo.config.js';
-import { createShortUrlAuth, redirectFromShortUrl } from './src/controller/short_url.controller.js';
+import { createShortUrlAuth, redirectFromShortUrl , createShortUrl} from './src/controller/short_url.controller.js';
 import { errorHandler } from './errorHandler.js';
 import auth_routes from './src/routes/auth.routes.js';
 import { authMiddleware } from './src/middleware/auth.middleware.js';
@@ -27,7 +27,9 @@ app.use(attachUser)
 
 app.use("/api/auth", auth_routes);
 app.use('/api/user', user_routes)
-app.post('/api/create', authMiddleware, createShortUrlAuth)
+// app.post('/api/create', authMiddleware, createShortUrlAuth)
+app.post('/api/create',  createShortUrl)
+
 app.get('/:id', redirectFromShortUrl)
 
 
